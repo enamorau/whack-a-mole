dabada5d7676a1a687c86308f711d0d96a4979d0
enamorau <msadki@me.com>
2018-10-30 21:08:41 +0100

    Moles are appearing randomly and do not overlap / Gameover is defined

--- JS/gameLogic.js
+++ JS/gameLogic.js
@@ -1,11 +1,29 @@
-// create a stargame function
+// create a game function
+var gameStatus = false;
+
+var startGame = function() {
+  setTimeout(moleTimer, 1000);
+  gameStatus = true;
+};
+var gameOver = function() {
+  this.canvas = document.getElementById("field");
+  this.ctx = this.canvas.getContext("2d");
+  alert("Gameover !");
+  this.cumulaM = 0;
+  this.molexTotal = [];
+  this.moleyTotal = [];
+  this.ctx.clearRect(0, 0, 500, 500);
+  gamestatus = false;
+  moleTimer.i = 0
+};
 
 // TODO creating a Gameplay object
 
 // TODO creating a player object
 
 function Player() {
-  this.area = 50;
+  this.arearadius = 50;
+  this.attackMoles = function() {};
 }
 
 // TODO creating a Mole object
@@ -17,12 +35,16 @@ function Mole() {
   this.x = moleXGenerator();
   this.y = moleYGenerator();
 
+  this.total = 0;
+
   this.draw = function() {
     var img = new Image();
     img.onload = function() {
       this.ctx.drawImage(img, this.x, this.y, 34, 56);
     }.bind(this);
     img.src = "./img/1Mole.png";
+    molexTotal.push(this.x);
+    moleyTotal.push(this.x);
   };
 }
 
@@ -38,19 +60,48 @@ var moleYGenerator = function() {
   return moleY;
 };
 
-var cumulaT = [];
+var cumulaM = 0;
+
+var cumulaT = 0;
+
+var molexTotal = [];
+
+var moleyTotal = [];
+
+//time functions
 
 randomTimeL1 = function() {
-    var randT = Math.floor(Math.random() * 2000);
-    
-    if (randT < 1000) { 
-      cumulaT.push(1000);
-      return 1000
-   }
-   else {
-      cumulaT.push(randT)
-      return randT;
-    }
-    
-  };
+  var randT = Math.floor(Math.random() * 2000);
+
+  if (randT < 1000) {
+    cumulaT.push(1000);
+    return 1000;
+  } else {
+    cumulaT.push(randT);
+    return randT;
+  }
+};
+
+function moleTimer() {
+  var rand = Math.round(Math.random() * 500); //+ 500;
+  this.i = 0
   
+  do {
+    var newMole = new Mole();
+    i ++;
+  } while (
+    ((molexTotal[i] < (newMole.x + 150)) && (molexTotal[i] > (newMole.x - 150))) ||
+    ((molexTotal[i] < (newMole.x + 150)) && (molexTotal[i] > (newMole.x - 150)))
+  );
+
+  newMole.draw();
+
+  cumulaT += rand;
+  cumulaM++;
+  console.log(newMole.x );
+  if (cumulaM >= 10) {
+    gameOver();
+  } else {
+    setTimeout(moleTimer, rand);
+  }
+}
--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,3 @@
-var onClickMole = function() {
-  (function loop() {
-    var rand = Math.round(Math.random() * (800 - 500)) + 500;
-    setTimeout(function() {
-      var newMole = new Mole();
-      newMole.draw();
-      loop();
-    }, rand);
-  })();
-};
-
 $(document).ready(function() {
-  $("#start-game").click(onClickMole);
+  $("#start-game").click(startGame);
 });
